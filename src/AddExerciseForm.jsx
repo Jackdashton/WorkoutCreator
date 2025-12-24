6d899efb21df8be1f7ad039b4a24c1bf328e4edd
Jack Ashton <jackdashton@outlook.com>
2025-12-24 19:13:25 +0000

    Handling for form set up and linked to handleSubmit

--- src/AddExerciseForm.jsx
+++ src/AddExerciseForm.jsx
@@ -1,18 +1,33 @@
+import { useState } from "react";
+
 function AddExerciseForm() {
+
+    const [name, setName] = useState("");
+    const [sets, setSets] = useState("");
+    const [reps, setReps] = useState("");
+    const [isWarmUp, setisWarmUp] = useState(false);
+
+    const handleSubmit = () => {
+        alert("connected");
+    }
+
     return (
         <>
-            <label>
-                Name: <input name="exerciseName"></input>
-            </label>
-            <label>
-                Sets: <input name="exerciseSets"></input>
-            </label>
-            <label>
-                Reps: <input name="exerciseReps"></input>
-            </label>
-            <label>
-                Warm Up Set?: <input type="checkbox" name="isWarmUp"></input>
-            </label>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name: <input value={name} onChange={(e) => setName(e.target.value)}></input>
+                </label>
+                <label>
+                    Sets: <input value={sets} onChange={(e) => setSets(e.target.value)}></input>
+                </label>
+                <label>
+                    Reps: <input value={reps} onChange={(e) => setReps(e.target.value)}></input>
+                </label>
+                <label>
+                    Warm Up Set?: <input type="checkbox" checked={isWarmUp} onChange={(e) => setisWarmUp(e.target.checked)}></input>
+                </label>
+                <button type="submit">Add Exercise</button>
+            </form>
         </>
     );
 }
